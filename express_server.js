ba07b211e283eeb16d231d964674b8cc6f8b835d
osmanmm <mohamed.osman@outlook.com>
2021-09-08 00:17:30 +0000

    remove end point set and fetch

--- express_server.js
+++ express_server.js
@@ -16,6 +16,6 @@ app.get("/urls.json", (req, res) => {
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
