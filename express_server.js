6027c68e421b2c1d796b0a92d5945269e6b8d24a
osmanmm <mohamed.osman@outlook.com>
2021-09-10 16:31:42 +0000

    add login button to the main urls page

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const PORT = 8080; // default port 8080
@@ -10,8 +12,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,7 +24,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+  
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
@@ -48,6 +53,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
    const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+
 });
 app.post("/urls/:shortURL", (req, res) => {
     const shortURL = req.params.shortURL;
@@ -55,6 +61,11 @@ app.post("/urls/:shortURL", (req, res) => {
     //res.redirect(`/urls/${shortURL}`);
     res.redirect('/urls');
   });
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    res.cookie("username" , username);
+    res.redirect("/urls");
+  });  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
